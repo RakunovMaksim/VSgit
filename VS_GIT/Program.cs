64a31d84f65dcd545e9cf77025e6a8b4dadd3650
Макс Ракунов <maks.rakunov73@gmail.com>
2025-02-25 21:24:11 +0400

    Добавление меню.

--- VS_GIT/Program.cs
+++ VS_GIT/Program.cs
@@ -1,15 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Выберите действие:");
-        Console.WriteLine("1. Ввести A");
-        Console.WriteLine("2. Ввести B");
-        Console.WriteLine("3. Выполнить операцию '+'");
-        Console.WriteLine("4. Выполнить операцию '-'");
-        Console.WriteLine("5. Выполнить операцию '*'");
-        Console.WriteLine("6. Выполнить операцию '/'");
-    }
-}
+﻿
